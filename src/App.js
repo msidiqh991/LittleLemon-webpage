c11b43694538e42994251c53bff198e7d56c8741
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2025-03-08 04:58:25 +0700

    update: deployment on GitHub

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Nav from "./components/Nav";
-import Main from "./components/Main";
+import Main from "./components/Main"
 import Menu from "./components/Menu";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
+  useEffect(() => {
+    document.title = "Little Lemon App"
+  }, [])
+
   return (
     <>
       <Nav />
+      <Header/>
       <Main />
       <Menu/>
       <Footer/>
